869d0463cf3ee9c30b4660e46c3e1acebd3631cd
Jake Anderson <jake94a@gmail.com>
2021-08-24 10:28:47 -0600

    header component

---
+++ src/Title.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Title.css";
+
+const Title = () => {
+  return (
+    <div className="Title">
+      <h1>hello title</h1>
+      <br />
+      <div className="Title-Subtitle">hello subtitle</div>
+    </div>
+  );
+};
+
+export default Title;
